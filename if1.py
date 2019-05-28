0a492ab06bce5898bfebdb9b5e1e46aea81e4cdf
Roman Kosolapov <kosolapov.re@gmail.com>
2019-05-28 23:39:13 +0300

    исправил функцию, если вводишь значение 100. Убрал abs перед приведением в int аргумента years

--- if1.py
+++ if1.py
@@ -1,6 +1,6 @@
 def main():
     years = input('Привет дорогой друг, напиши свой возраст и я угадаю, чем ты занимаешься!')
-    years = abs(int(years))
+    years = int(years)
     if years <= 6:
         print('Ты ходишь в детский сад!')
     elif years <= 17:
@@ -9,7 +9,7 @@ def main():
         print('Ты учишься в ВУЗе!')
     elif years <= 65:
         print('Ты ходишь на работу!')
-    elif years < 100:
+    elif years <= 100:
         print('После 65 лет ты не можешь работать, ты пенсионер!')
     elif years > 100:
         print('Так долго люди не живут, ты иноплянетянин!')
