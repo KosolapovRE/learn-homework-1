f8cbeca19d88ff866fe7b7048a81d2c2864901b0
Roman Kosolapov <kosolapov.re@gmail.com>
2019-05-30 22:58:59 +0300

    Переделал домашку согласно комментам Артема. 1. написать функцию, которая принимает аргумент years. 2. Решить вопрос с невалидными значениями

--- if1.py
+++ if1.py
@@ -1,19 +1,32 @@
+
 def main():
     years = input('Привет дорогой друг, напиши свой возраст и я угадаю, чем ты занимаешься!')
-    years = int(years)
-    if years <= 6:
-        print('Ты ходишь в детский сад!')
-    elif years <= 17:
-        print('Ты учишься в школе!')
-    elif years <= 23:
-        print('Ты учишься в ВУЗе!')
-    elif years <= 65:
-        print('Ты ходишь на работу!')
-    elif years <= 100:
-        print('После 65 лет ты не можешь работать, ты пенсионер!')
-    elif years > 100:
-        print('Так долго люди не живут, ты иноплянетянин!')
-    return print(years)
-    
+
+
+    def what_you_do(years):
+        try:
+            years = int(years)
+            if years <= 0:
+                return 'Ты еще не родился!'
+            elif years <= 6:
+                return 'Ты ходишь в детский сад!'
+            elif years <= 17:
+                return 'Ты учишься в школе!'
+            elif years <= 23:
+                return 'Ты учишься в ВУЗе!'
+            elif years <= 65:
+                return 'Ты ходишь на работу!'
+            elif years <= 100:
+                return 'После 65 лет ты не можешь работать, ты пенсионер!'
+            elif years > 100:
+                return 'Так долго люди не живут, ты иноплянетянин!'
+        except (ValueError, TypeError):
+            return 'Вы неправильно ввели возраст'
+       
+
+    answer = what_you_do(years)
+    print(answer)
+
+
 if __name__ == "__main__": 
     main()
