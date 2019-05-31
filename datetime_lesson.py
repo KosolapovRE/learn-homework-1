e3764cbf86fad8b57893e68af0a42691a142dcc1
Roman Kosolapov <kosolapov.re@gmail.com>
2019-05-31 23:00:08 +0300

    исправил модуль Locale. Неправильно писал применение русского языка

--- datetime_lesson.py
+++ datetime_lesson.py
@@ -1,6 +1,6 @@
 from datetime import date, timedelta
 import locale
-locale.setlocale(locale.LC_ALL, "russian")
+locale.setlocale(locale.LC_ALL, 'ru_RU')
 
 dt_today = date.today()
 print(dt_today)
@@ -14,3 +14,4 @@ from datetime import datetime
 dt_str = "01/01/17 12:10:03.234567"
 dt_new = datetime.strptime(dt_str, '%m/%d/%y %H:%M:%S.%f')
 print(dt_new)
+print(dt_new.strftime('%A %d %B %Y'))
