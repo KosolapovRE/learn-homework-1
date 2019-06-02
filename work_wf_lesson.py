8a00306b1d80184347b2555fab34f1eb62a042a1
Roman Kosolapov <kosolapov.re@gmail.com>
2019-06-02 14:46:29 +0300

    Написал пробный вариант вычисления количества слов в тексте и отправил на проверку

--- work_wf_lesson.py
+++ work_wf_lesson.py
@@ -1,4 +1,14 @@
 with open('referat.txt', 'r', encoding='utf8') as text:
     text = text.read()
-    print(text)
     print(len(text))
+
+text = text.replace('\n', ' ').replace('.', '!')
+text = text.split(' ')
+punctuation = [',', '.', ':', '«', '»', '!', ' ']
+
+for element in text:
+    if element == punctuation:
+        del text[element]
+
+print(text)
+print(len(text))
