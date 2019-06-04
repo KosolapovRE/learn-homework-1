92a373ada2d421b456797abdf82b3dbac4a24279
Roman Kosolapov <kosolapov.re@gmail.com>
2019-06-04 08:59:05 +0300

    сделал согласно рекомендациям Артема

--- work_wf_lesson.py
+++ work_wf_lesson.py
@@ -1,14 +1,15 @@
-with open('referat.txt', 'r', encoding='utf8') as text:
-    text = text.read()
-    print(len(text))
+with open('referat.txt', 'r', encoding='utf8') as a:
+    text = a.read()
 
-text = text.replace('\n', ' ').replace('.', '!')
-text = text.split(' ')
-punctuation = [',', '.', ':', '«', '»', '!', ' ']
+text_len = str(len(text))
+text = text.replace('.', '!')
+text_list_len = str(len(text.split(' ')))
 
-for element in text:
-    if element == punctuation:
-        del text[element]
-
-print(text)
-print(len(text))
+with open('referat2.txt', 'w', encoding='utf8') as new_text:
+    new_text.write(text)
+    new_text.write('\n')
+    new_text.write(text_len)
+    new_text.write('\n')
+    new_text.write(text_list_len)
+        
+        
